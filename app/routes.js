81ace23e7068ae1612f36b9df0b5abe7f6639b55
colin <lilonglongfwz@gmail.com>
2018-06-25 15:08:35 +0900

    [view] add new view for events

--- app/routes.js
+++ app/routes.js
@@ -2,10 +2,14 @@
 
 const express = require('express'),
   router = express.Router(),
-  mainController = require('./controllers/main.controller');
+  mainController = require('./controllers/main.controller'),
+  eventsController = require('./controllers/events.controller');
 
 // export router
 module.exports = router;
 
 //define routes
 router.get('/', mainController.showHome);
+
+//define routes
+router.get('/events', eventsController.showEvents);
